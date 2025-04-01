4f1fb512153b91ebfa4314004c654dbd8a6b0780
Braidon <braidonbrauer@gmail.com>
2025-04-01 15:49:04 +0900

    Adds color palletts in an imported object.

--- app/src/main/java/com/example/sentencesstudyapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/sentencesstudyapp/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+object AppColors {
+    val Background = Color(0xFF27323A)
+    val Foreground = Color(0xFF435055)
+    val Lowlight = Color(0xFF29A19C)
+    val Highlight = Color(0xFFA3F7BF)
+    val Caution = Color(0xFFFE4F2D)
+    val Text = Color(0xFFFFFFFF)
+}
