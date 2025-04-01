f0ce813109e1fdde6d9e85a6bae5f95172a762af
BraidonB <111916207+BraidonB@users.noreply.github.com>
2025-04-01 15:41:05 +0900

    Revert "Initial prompt box stage."

--- app/src/main/java/com/example/sentencesstudyapp/MainActivity.kt
+++ app/src/main/java/com/example/sentencesstudyapp/MainActivity.kt
@@ -4,23 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.sentencesstudyapp.ui.theme.SentencesStudyAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,34 +19,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SentencesStudyAppTheme {
-
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
 }
 
 @Composable
-fun PromptBox() {
-    LazyRow(
-        horizontalArrangement = Arrangement.Start,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        item {
-            Text(
-                modifier = Modifier.padding(16.dp),
-                text = "日本語の文",
-                textAlign = TextAlign.Center,
-                fontSize = 30.sp,
-                color = Color.White
-            )
-        }
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun PromptPreview() {
+fun GreetingPreview() {
     SentencesStudyAppTheme {
-        PromptBox()
+        Greeting("Android")
     }
 }
